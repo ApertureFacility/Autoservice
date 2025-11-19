161f2b5f34c9790ad73ae653cb835035d6b16fbd
Sergey FrontendDeveloper <sergeyt.devwizard@gmail.com>
2025-11-19 22:24:59 +0300

    (Refactor) removed data massive from files to solo files

---
+++ app/avtovykup/data/CarsPhotoDamaged.ts
@@ -0,0 +1,67 @@
+import { IDamagedCars } from "../interfaces";
+
+ export const DamagedCars: IDamagedCars[] = [
+    {
+      name: "Ford Galaxy II",
+      img: "/cars/car1.png",
+      price: "300 000 ₽",
+      year: "2009",
+      mileage: "450 000 км",
+    },
+    {
+      name: "Ford Focus 2",
+      img: "/cars/focus2.jpg",
+      price: "350 000 ₽",
+      year: "2006",
+      mileage: "550 000 км",
+    },
+    {
+      name: "Ford Mondeo IV",
+      img: "/cars/car3.png",
+      price: "250 000 ₽",
+      year: "2006",
+      mileage: "350 000 км",
+    },
+    {
+      name: "Mercedes-Benz GLB I (X247)",
+      img: "/cars/mers2.jpg",
+      price: "35 000 $",
+      year: "2006",
+      mileage: "64 000 км",
+    },
+    {
+      name: "Toyota RAV4 V (XA50)",
+      img: "/cars/rav2.jpg",
+      price: "250 000 ₽",
+      year: "2019",
+      mileage: "350 000 км",
+    },
+    {
+      name: "Volkswagen Touareg III",
+      img: "/cars/touareg.jpg",
+      price: "250 000 ₽",
+      year: "2020",
+      mileage: "350 000 км",
+    },
+    {
+      name: "Volkswagen Polo Sedan II",
+      img: "/cars/polo.jpg",
+      price: "250 000 ₽",
+      year: "2015",
+      mileage: "350 000 км",
+    },
+    {
+      name: "Lexus IS II",
+      img: "/cars/lexus.jpg",
+      price: "250 000 ₽",
+      year: "2009",
+      mileage: "350 000 км",
+    },
+    {
+      name: "BMW 3 серия E90",
+      img: "/cars/bmw.jpeg",
+      price: "250 000 ₽",
+      year: "2008",
+      mileage: "350 000 км",
+    },
+  ];
